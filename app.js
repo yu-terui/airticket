e24f67a4bfe024f02c3f8b9d0f5e8adc3792054c
y.terui <sakulovesmileee@gmail.com>
2024-01-24 12:12:31 +0900

    レビュー新規追加機能

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const path = require('path');
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const mysql = require('mysql2');
 
@@ -17,11 +19,10 @@ const con = mysql.createConnection({
 
 // mysqlからデータを持ってくる
 app.get('/', (req, res) => {
-  // 20行あたりに適用してください。
 // cssファイルの取得
 app.use(express.static('assets'));
   const sql = "select * from personas";
-app.post('/', (req, res) => {
+app.post('/', (req, res) => { //データベースを変更したいときはpost
   const sql = "INSERT INTO personas SET ?"
   con.query(sql, req.body, function(err, result, fields) {
     if (err) throw err;
@@ -29,7 +30,6 @@ app.post('/', (req, res) => {
     res.redirect('/');
   });
 });
-
   con.query(sql, function (err, result, fields) {
     if (err) throw err;
     res.render('index', {
