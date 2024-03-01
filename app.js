d133204c0fc81c273d0f11f77e02eebc371c7866
y.terui <sakulovesmileee@gmail.com>
2024-03-01 12:00:47 +0900

    搭乗者情報入力ページ_バリデーション機能修正完了

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ con.connect(function (err) {
   if (err) throw err;
   console.log("Connected");
   let sql =
-    "create table if not exists members (id INT auto_increment not null,userName varchar(45),furigana varchar(45),address varchar(45),email varchar(45),password varchar(45),primary key(id));";
+    "create table if not exists members (id INT auto_increment not null,familyname varchar(45),firstname varchar(45),age INT,sex varchar(45),email varchar(45),password varchar(45),primary key(id));";
   con.query(sql, function (err, result, fields) {
     if (err) throw err;
     console.log("table created");
@@ -34,17 +34,17 @@ con.connect(function (err) {
 });
 // mysqlからデータを持ってくる
 app.get("/", (req, res) => {
-  const sql =
-    "select * from flights";
+  const sql = "select * from flights";
   con.query(sql, function (err, result, fields) {
     if (err) throw err;
     res.render("index", {
-      flights: result
+      flights: result,
     });
   });
 });
 app.post("/ticket_select/:departure", (req, res) => {
-  const sql = "SELECT * FROM flights left join tickets on tickets.flight_num = flights.flight_num where departure = ?";
+  const sql =
+    "SELECT * FROM flights left join tickets on tickets.flight_num = flights.flight_num where departure = ?";
   let data = req.body;
   con.query(sql, data.departure, function (err, result, fields) {
     if (err) throw err;
@@ -62,73 +62,72 @@ app.post("/ticket_check", (req, res) => {
   let data = req.body;
   con.query(sql, req.body, function (err, result, fields) {
     if (err) throw err;
-  res.render("ticket_check", {
-    date: data.date,
-    flight_num: data.flight_num,
-    departure: data.departure,
-    departure_time: data.departure_time,
-    arrival: data.arrival,
-    arrival_time: data.arrival_time,
-    type: data.type,
-    people: data.people,
-    total: data.total,
-});
-});
+    res.render("ticket_check", {
+      date: data.date,
+      flight_num: data.flight_num,
+      departure: data.departure,
+      departure_time: data.departure_time,
+      arrival: data.arrival,
+      arrival_time: data.arrival_time,
+      type: data.type,
+      people: data.people,
+      total: data.total,
+    });
+  });
 });
 app.get("/booking_search", (req, res) => {
-    res.render("booking_search");
+  res.render("booking_search");
 });
 app.get("/booking_check", (req, res) => {
-    res.render("booking_check");
+  res.render("booking_check");
 });
 app.post("/booking_info", (req, res) => {
   const sql = "SELECT * FROM flights";
   let data = req.body;
   con.query(sql, req.body, function (err, result, fields) {
     if (err) throw err;
-  res.render("booking_info", {
-    date: data.date,
-    flight_num: data.flight_num,
-    departure: data.departure,
-    departure_time: data.departure_time,
-    arrival: data.arrival,
-    arrival_time: data.arrival_time,
-    people: data.people,
-    total: data.total,
-});
-});
+    res.render("booking_info", {
+      date: data.date,
+      flight_num: data.flight_num,
+      departure: data.departure,
+      departure_time: data.departure_time,
+      arrival: data.arrival,
+      arrival_time: data.arrival_time,
+      people: data.people,
+      total: data.total,
+    });
+  });
 });
 app.post("/booking_confirm", (req, res) => {
-  // res.render("booking_confirm");
   res.send(req.body);
 });
 app.get("/member_login", (req, res) => {
-  res.render("member_login")
-})
+  res.render("member_login");
+});
 app.post("/member_login", (req, res) => {
   const sql = "SELECT * FROM flights";
   let data = req.body;
   con.query(sql, req.body, function (err, result, fields) {
     if (err) throw err;
-  res.render("member_login", {
-    date: data.date,
-    flight_num: data.flight_num,
-    departure: data.departure,
-    departure_time: data.departure_time,
-    arrival: data.arrival,
-    arrival_time: data.arrival_time,
-    people: data.people,
-    total: data.total,
-});
-});
+    res.render("member_login", {
+      date: data.date,
+      flight_num: data.flight_num,
+      departure: data.departure,
+      departure_time: data.departure_time,
+      arrival: data.arrival,
+      arrival_time: data.arrival_time,
+      people: data.people,
+      total: data.total,
+    });
+  });
 });
 app.get("/member_register", (req, res) => {
-    res.render("member_register");
+  res.render("member_register");
 });
 app.post("/member_register", (req, res) => {
-    res.render("member_register");
+  res.render("member_register");
 });
 app.get("/member_register_complete", (req, res) => {
-    res.render("member_register_complete");
+  res.render("member_register_complete");
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
