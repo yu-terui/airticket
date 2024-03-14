2b324d558b5342e0ea03241818a10df6184c1dd5
y.terui <sakulovesmileee@gmail.com>
2024-03-14 18:55:07 +0900

    会員ログイン画面ー認証機能OK.データ引き渡しNGパターン

--- login.js
+++ login.js
@@ -13,7 +13,7 @@ router.use(passport.initialize());
 router.use(passport.session());
 
 // ログイン画面
-router.post("/member_login", (req, res) => {
+router.get("/member_login", (req, res) => {
   const sql = "SELECT * FROM flights";
   let data = req.body;
   con.query(sql, req.body, function (err, result) {
@@ -32,7 +32,7 @@ router.post("/member_login", (req, res) => {
 });
 
 // ログイン後の遷移画面
-router.post("/booking_info", (req, res) => {
+router.get("/booking_info", (req, res) => {
   const sql = "SELECT * FROM flights";
   let data = req.body;
   con.query(sql, req.body, function (err, result) {
@@ -50,12 +50,31 @@ router.post("/booking_info", (req, res) => {
   });
 });
 
+//失敗したとき
+router.get("/member_login", (req, res) => {
+  const sql = "SELECT * FROM flights";
+  let data = req.body;
+  con.query(sql, req.body, function (err, result) {
+    if (err) throw err;
+    res.render("member_login", {
+      date: data.date,
+      flight_num: data.flight_num,
+      departure: data.departure,
+      departure_time: data.departure_time,
+      arrival: data.arrival,
+      arrival_time: data.arrival_time,
+      people: data.people,
+      total: data.total,
+    });
+  });
+});
+
 // ログイン分岐
 router.post(
   "/booking_info",
   passport.authenticate("local", {
     successRedirect: "/booking_info",
-    failureRedirect: "/fail",
+    failureRedirect: "/member_login",
   }),
   function (req, res) {
     res.redirect("/");
@@ -63,74 +82,60 @@ router.post(
 );
 
 // ログイン処理
-passport.use(new LocalStrategy(
+passport.use(
+  new LocalStrategy(
     {
       // inputのnameと合わせる
       usernameField: "id",
-    passwordField: "password",
-    session: true,
-    passReqToCallback: false,
+      passwordField: "password",
+      session: true,
+      passReqToCallback: false,
     },
     function (input_id, input_password, done) {
-      const sql = `select * from members where id = '${input_id}'`
+      const sql = `select * from members where id = '${input_id}'`;
       con.query(sql, function (err, result) {
-      login_data = result[0]
-      if (err) return done(err)
-      if (!login_data) return done(null, false, { message: 'account does not exist' })
-      if (input_password == login_data.password) {
-        return done(null, login_data); // ログイン成功
-      } else {
-        return done(null, false, { message: "wrong password" });
-      }
-      })
+        login_data = result[0];
+        if (err) return done(err);
+        if (!login_data)
+          return done(null, false, { message: "account does not exist" });
+        if (input_password == login_data.password) {
+          return done(null, login_data); // ログイン成功
+        } else {
+          return done(null, false, { message: "wrong password" });
+        }
+      });
     }
   )
 );
 
 // ログインに成功したらuser.idのセッションを生成し、Cookieを作る
 passport.serializeUser(function (user, done) {
-  done(null, user.id)
+  done(null, user.id);
 });
 
 passport.deserializeUser(function (user_id_saved, done) {
   // ユーザーの情報をDBから探す
-  const sql = `select * from members where id = '${user_id_saved}'`
+  const sql = `select * from members where id = '${user_id_saved}'`;
   con.query(sql, function (err, result) {
-      done(null, result[0])
-  })
+    done(null, result[0]);
+  });
 });
 
 // ミドルウェアでログイン有無によるページ接近の管理
 function is_login(req, res, next) {
   if (req.user) {
-      // loginした状態なら、通す
-      next()
+    // loginした状態なら、通す
+    next();
   } else {
-      // loginしてないならこのページに飛ばす
-      res.render('member_login.ejs')
+    // loginしてないならこのページに飛ばす
+    res.render("member_login.ejs");
   }
 }
 
 // ミドルウェアでログインしているかチェックし、ログインしているならreq.userのDBを見せる
-router.get('/booking_info', is_login, function (req, res) {
-  // deserializeUserで得られたuserのDBデータ持ってくる
-  // const sql = "SELECT * FROM flights";
-  // let data = req.body;
-  // con.query(sql, req.body, function (err, result) {
-  //   if (err) throw err;
-  //   res.render("booking_info", {
-  //     date: data.date,
-  //     flight_num: data.flight_num,
-  //     departure: data.departure,
-  //     departure_time: data.departure_time,
-  //     arrival: data.arrival,
-  //     arrival_time: data.arrival_time,
-  //     people: data.people,
-  //     total: data.total,
-  //   });
-  // });
-  console.log(req.user)
-  res.send(req.user)
-})
+// router.get("/booking_info", is_login, function (req, res) {
+//   // console.log(req.user)
+//   // res.send(req.user)
+// });
 
 module.exports = router;
